e429a10b4bedf326e25e65ab0fdb41d56df48ca1
Sonia Ruiz <yune.vk@gmail.com>
2018-11-04 21:35:14 +0100

    Change clouds

--- js/cloud.js
+++ js/cloud.js
@@ -4,29 +4,28 @@ function Cloud(){
 	
 	// create a cube geometry;
 	// this shape will be duplicated to create the cloud
-	var geom = new THREE.BoxGeometry(20,20,20);
+	var geom = new THREE.OctahedronGeometry(10,2);
 	
 	// create a material; a simple white material will do the trick
-	var mat = new THREE.MeshPhongMaterial({
-		color:Colors.white,  
-	});
+	var mat = new THREE.MeshPhongMaterial( {
+		color: 0xffffff,
+		flatShading: true
+} );
 	
 	// duplicate the geometry a random number of times
-	var nBlocs = 3+Math.floor(Math.random()*3);
+	var nBlocs = 5+Math.floor(Math.random()*3);
 	for (var i=0; i<nBlocs; i++ ){
 		
 		// create the mesh by cloning the geometry
 		var mesh = new THREE.Mesh(geom, mat); 
 		
 		// set the position and the rotation of each cube randomly
-		mesh.position.x = i*15;
-		mesh.position.y = Math.random()*10;
-		mesh.position.z = Math.random()*10;
-		mesh.rotation.z = Math.random()*Math.PI*2;
-		mesh.rotation.y = Math.random()*Math.PI*2;
+		mesh.position.x = i*5;
+		mesh.position.y = Math.random()*1;
+		mesh.position.z = Math.random()*15;
 		
 		// set the size of the cube randomly
-		var scale = .1 + Math.random()*.9;
+		var scale = .7 + Math.random()*.9;
 		mesh.scale.set(scale,scale,scale);
 		
 		// allow each cube to cast and to receive shadows
--- js/game.js
+++ js/game.js
@@ -103,7 +103,8 @@ Game.prototype.loop = function () {
   // Rotate the kokoro, the sea and the sky
   //this.kokoro.propeller.rotation.x += 0.3;
   this.ground.mesh.rotation.z += .005;
-  this.sky.mesh.rotation.z += .01;
+  //this.sky.mesh.rotation.z += .01;
+  this.sky.mesh.rotation.z += .001;
 
   // render the scene
   this.renderer.render(this.scene, this.camera);
