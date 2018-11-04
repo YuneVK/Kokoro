446c290fc5f869dc075a13789966c77594ec27f3
Sonia Ruiz <yune.vk@gmail.com>
2018-11-04 21:14:09 +0100

    Add Kokoro to game

--- js/game.js
+++ js/game.js
@@ -101,7 +101,7 @@ Game.prototype.handleWindowResize = function() {
 
 Game.prototype.loop = function () {
   // Rotate the kokoro, the sea and the sky
-  this.kokoro.propeller.rotation.x += 0.3;
+  //this.kokoro.propeller.rotation.x += 0.3;
   this.ground.mesh.rotation.z += .005;
   this.sky.mesh.rotation.z += .01;
 
--- js/kokoro.js
+++ js/kokoro.js
@@ -11,58 +11,136 @@ function Kokoro(scene) {
 };
 
 Kokoro.prototype.generateModel = function() {
-  // Create the cabin
-  var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
-  var matCockpit = new THREE.MeshPhongMaterial({color:Colors.red, flatShading:true});
-  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
-  cockpit.castShadow = true;
-  cockpit.receiveShadow = true;
-  this.mesh.add(cockpit);
 
-  // Create the engine
-  var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
-  var matEngine = new THREE.MeshPhongMaterial({color:Colors.white, flatShading:true});
-  var engine = new THREE.Mesh(geomEngine, matEngine);
-  engine.position.x = 40;
-  engine.castShadow = true;
-  engine.receiveShadow = true;
-  this.mesh.add(engine);
 
-  // Create the tail
-  var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
-  var matTailPlane = new THREE.MeshPhongMaterial({color:Colors.red, flatShading:true});
-  var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
-  tailPlane.position.set(-35,25,0);
-  tailPlane.castShadow = true;
-  tailPlane.receiveShadow = true;
-  this.mesh.add(tailPlane);
 
-  // Create the wing
-  var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
-  var matSideWing = new THREE.MeshPhongMaterial({color:Colors.red, flatShading:true});
-  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
-  sideWing.castShadow = true;
-  sideWing.receiveShadow = true;
-  this.mesh.add(sideWing);
+  groupHead = new THREE.Object3D();
 
-  // propeller
-  var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
-  var matPropeller = new THREE.MeshPhongMaterial({color:Colors.brown, flatShading:true});
-  this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
-  this.propeller.castShadow = true;
-  this.propeller.receiveShadow = true;
+  /* BEAK */
+  var geometryBeak = new THREE.ConeBufferGeometry(25, 100, 32);
+  var materialBeak = new THREE.MeshPhongMaterial({
+    color: 0x212121,
+    flatShading: true
+  });
+  var beak = new THREE.Mesh(geometryBeak, materialBeak);
+  beak.position.y = 100;
+  beak.position.x = 30;
+  beak.rotation.z = THREE.Math.degToRad(-30);
+  groupHead.add(beak);
 
-  // blades
-  var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
-  var matBlade = new THREE.MeshPhongMaterial({color:Colors.brownDark});
 
-  var blade = new THREE.Mesh(geomBlade, matBlade);
-  blade.position.set(8,0,0);
-  blade.castShadow = true;
-  blade.receiveShadow = true;
-  this.propeller.add(blade);
-  this.propeller.position.set(50,0,0);
-  this.mesh.add(this.propeller);
+  /* HEAD */
+  var geometryHead = new THREE.CylinderGeometry(30, 30, 50, 32);
+  var materialHead = new THREE.MeshPhongMaterial({
+    color: 0x212121,
+    flatShading: true
+  });
+  var head = new THREE.Mesh(geometryHead, materialHead);
+  head.castShadow = true;
+  head.receiveShadow = true;
+  head.position.y = 50;
+  head.rotation.x = Math.PI / 2;
+  groupHead.add(head);
+
+  /* EYE 1 */
+  var geometryEye1 = new THREE.CircleGeometry(20, 20);
+  var materialEye1 = new THREE.MeshBasicMaterial({
+    color: 0xffffff
+  });
+  var eye1 = new THREE.Mesh(geometryEye1, materialEye1);
+  eye1.position.z = 26;
+  eye1.position.y = 50;
+  groupHead.add(eye1);
+
+  /* EYE 2 */
+  var geometryEye2 = new THREE.CircleGeometry(20, 20);
+  var materialEye2 = new THREE.MeshBasicMaterial({
+    color: 0xffffff
+  });
+  var eye2 = new THREE.Mesh(geometryEye2, materialEye2);
+  eye2.material.side = THREE.DoubleSide; // to show all sides
+  eye2.position.z = -26;
+  eye2.position.y = 50;
+  groupHead.add(eye2);
+
+  /* EYE 1 BLACK*/
+  var geometryEye1Black = new THREE.CircleGeometry(15, 15);
+  var materialEye1Black = new THREE.MeshBasicMaterial({
+    color: 0x212121
+  });
+  var eye1Black = new THREE.Mesh(geometryEye1Black, materialEye1Black);
+  eye1Black.position.z = 27;
+  eye1Black.position.y = 50;
+  groupHead.add(eye1Black);
+
+  /* EYE 2 BLACK */
+  var geometryEye2Black = new THREE.CircleGeometry(15, 15);
+  var materialEye2Black = new THREE.MeshBasicMaterial({
+    color: 0x212121
+  });
+  var eye2Black = new THREE.Mesh(geometryEye2Black, materialEye2Black);
+  eye2Black.material.side = THREE.DoubleSide; // to show all sides
+  eye2Black.position.z = -27;
+  eye2Black.position.y = 50;
+  groupHead.add(eye2Black);
+
+  this.mesh.add(groupHead);
+
+
+  groupBody = new THREE.Object3D();
+
+  /* NECK */
+  var geometryNeck = new THREE.CylinderGeometry(10, 10, 50, 32, 5);
+  var materialNeck = new THREE.MeshPhongMaterial({
+    color: 0x212121,
+    flatShading: true
+  });
+  var neck = new THREE.Mesh(geometryNeck, materialNeck);
+  neck.castShadow = true;
+  neck.receiveShadow = true;
+  neck.rotation.z = THREE.Math.degToRad(25);
+  neck.position.x = 10;
+  neck.position.y = 10;
+  groupBody.add(neck);
+
+  /* BODY */
+  var geometryBody = new THREE.CylinderGeometry(10, 40, 100, 32, 5);
+  var materialBody = new THREE.MeshPhongMaterial({
+    color: 0x212121,
+    flatShading: true
+  });
+  var body = new THREE.Mesh(geometryBody, materialBody);
+  body.castShadow = true;
+  body.receiveShadow = true;
+  body.rotation.z = THREE.Math.degToRad(25);
+  body.position.y = -45;
+  body.position.x = 35;
+  groupBody.add(body);
+
+  this.mesh.add(groupBody);
+
+
+  /* WING 1 */
+  var geometryWing1 = new THREE.CircleGeometry(85, 32, 0, .8);
+  var materialWing1 = new THREE.MeshBasicMaterial({
+    color: 0x212121
+  });
+  wing1 = new THREE.Mesh(geometryWing1, materialWing1);
+  wing1.material.side = THREE.DoubleSide; // to show all sides
+  wing1.position.x = 10;
+  wing1.position.y = -10;
+  wing1.rotation.y = THREE.Math.degToRad(180);
+  wing1.rotation.x = THREE.Math.degToRad(180);
+  wing1.rotation.z = THREE.Math.degToRad(20);
+  this.mesh.add(wing1);
+
+  /*
+  var radians = THREE.Math.degToRad( degrees );
+  var degrees = THREE.Math.radToDeg( radians );
+  */
+
+  this.mesh.rotation.z = THREE.Math.degToRad(280);
+
 }
 
 Kokoro.prototype.addToScene = function() {
